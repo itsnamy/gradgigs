8f8b51d71bd3cbc2a92f0069982d90f8c02e7c67
AHMAD DARWISH DANIAL BIN AHMAD NAZRUN <adarwush@gmail.com>
2024-04-24 17:02:09 +0800

    job seeker profile

--- lib/view/user_profile/apl_profile.dart
+++ lib/view/user_profile/apl_profile.dart
@@ -6,6 +6,8 @@ import 'package:gradgigs/view/user_profile/apl_basic_information.dart';
 import 'package:gradgigs/view/user_profile/apl_supporting_documents.dart';
 import 'package:gradgigs/view/user_profile/apl_contact_details.dart';
 
+// ignore_for_file: prefer_const_constructors
+
 class ApplicantProfilePage extends StatefulWidget {
   const ApplicantProfilePage({super.key});
 
@@ -26,7 +28,7 @@ class _ApplicantProfilePageState extends State<ApplicantProfilePage> {
             fontSize: 24,
           ),
         ),
-        toolbarHeight: 90,
+        toolbarHeight: 60,
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.only(
             bottomRight: Radius.circular(25),
@@ -47,142 +49,180 @@ class _ApplicantProfilePageState extends State<ApplicantProfilePage> {
           },
         ),
       ),
-
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 20.0, 0, 20.0),
-        child: Center(
-          child: Container(
-            height: 600,
-            width: 400,
-            decoration: BoxDecoration(
-              color: Colors.brown[400],
-              borderRadius: BorderRadius.circular(20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+          child: Center(
+              child: Column(children: [
+            Container(
+              height: 550,
+              width: 400,
+              decoration: BoxDecoration(
+                color: Colors.brown[400],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Align(
+                    alignment: AlignmentDirectional(0, -1),
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                          'https://picsum.photos/seed/890/600',
+                          width: 150,
+                          height: 150,
+                          fit: BoxFit.none,
+                          alignment: Alignment(0, -1),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Full name',
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Job seeker',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  TextButton(
+                    onPressed: () => applicantBasicInformation(context),
+                    child: const Text(
+                      'Basic Information',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                  TextButton(
+                    onPressed: () => applicantAcademicInformation(context),
+                    child: const Text(
+                      'Academic Information',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                  TextButton(
+                    onPressed: () => applicantContactDetails(context),
+                    child: const Text(
+                      'Contact Details',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                  TextButton(
+                    onPressed: () => applicantBankDetails(context),
+                    child: const Text(
+                      'Bank Details',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                  TextButton(
+                    onPressed: () => applicantSupportingDocuments(context),
+                    child: const Text(
+                      'Supporting Documents',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                  Text(
+                    'Settings',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ), // Spacer
+                ],
+              ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () => applicantBasicInformation(context),
-                  child: const Text(
-                    'Basic Information',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
+            SizedBox(height: 10),
+            SizedBox(
+              width: 400,
+              child: ElevatedButton(
+                onPressed: () {
+                  loginPage(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  backgroundColor: const Color.fromARGB(255, 228, 185, 112),
+                ),
+                child: const Column(
+                  children: [
+                    Text(
+                      'Log out',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 61, 61, 61),
+                        fontFamily: 'Contrail One',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-                const SizedBox(height: 20),
-                TextButton(
-                  onPressed: () => applicantAcademicInformation(context),
-                  child: const Text(
-                    'Academic Information',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextButton(
-                  onPressed: () => applicantContactDetails(context),
-                  child: const Text(
-                    'Contact Details',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextButton(
-                  onPressed: () => applicantBankDetails(context),
-                  child: const Text(
-                    'Bank Details',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextButton(
-                  onPressed: () => applicantSupportingDocuments(context),
-                  child: const Text(
-                    'Supporting Documents',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Text (
-                  'Settings',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 20), // Spacer
-                
-              ],
+              ),
             ),
-          ),
-
-          // child: ElevatedButton(
-          //       onPressed: () {
-          //         loginPage(context);
-          //       },
-          //       style: ElevatedButton.styleFrom(
-          //         padding:
-          //             const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-          //         shape: RoundedRectangleBorder(
-          //           borderRadius: BorderRadius.circular(10),
-          //         ),
-          //         backgroundColor: const Color.fromARGB(255, 228, 185, 112),
-          //       ),
-          //       child: const Column(
-          //         children: [
-          //           Text(
-          //             'Log out',
-          //             style: TextStyle(
-          //               color: Color.fromARGB(255, 61, 61, 61),
-          //               fontFamily: 'Contrail One',
-          //               fontWeight: FontWeight.bold,
-          //               fontSize: 18,
-          //             ),
-          //           ),
-          //           SizedBox(height: 10),
-          //         ],
-          //       ),
-          //     ),
-
+          ])),
         ),
       ),
     );
   }
 }
 
-void applicantBasicInformation(BuildContext context){
-  Navigator.push(context, MaterialPageRoute(builder: (context) => ApplicantBasicInformation()));
+void applicantBasicInformation(BuildContext context) {
+  Navigator.push(context,
+      MaterialPageRoute(builder: (context) => ApplicantBasicInformation()));
 }
 
-void applicantAcademicInformation(BuildContext context){
-  Navigator.push(context, MaterialPageRoute(builder: (context) => ApplicantAcademicInformation()));
+void applicantAcademicInformation(BuildContext context) {
+  Navigator.push(context,
+      MaterialPageRoute(builder: (context) => ApplicantAcademicInformation()));
 }
 
-void applicantContactDetails(BuildContext context){
-  Navigator.push(context, MaterialPageRoute(builder: (context) => ApplicantContactDetails()));
+void applicantContactDetails(BuildContext context) {
+  Navigator.push(context,
+      MaterialPageRoute(builder: (context) => ApplicantContactDetails()));
 }
 
-void applicantBankDetails(BuildContext context){
-  Navigator.push(context, MaterialPageRoute(builder: (context) => ApplicantBankDetails()));
+void applicantBankDetails(BuildContext context) {
+  Navigator.push(
+      context, MaterialPageRoute(builder: (context) => ApplicantBankDetails()));
 }
 
-void applicantSupportingDocuments(BuildContext context){
-  Navigator.push(context, MaterialPageRoute(builder: (context) => ApplicantSupportingDocuments()));
+void applicantSupportingDocuments(BuildContext context) {
+  Navigator.push(context,
+      MaterialPageRoute(builder: (context) => ApplicantSupportingDocuments()));
 }
 
-void loginPage(BuildContext context){
-  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage(title: 'Sign In')));
+void loginPage(BuildContext context) {
+  Navigator.push(context,
+      MaterialPageRoute(builder: (context) => LoginPage(title: 'Sign In')));
 }
