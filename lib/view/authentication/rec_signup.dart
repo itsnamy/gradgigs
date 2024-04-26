155a40b5b3911057289a43e22d174832cdf52bb0
Afifah <afifah02@graduate.utm.my>
2024-04-26 14:31:11 +0800

    apl & rec signup

--- lib/view/authentication/rec_signup.dart
+++ lib/view/authentication/rec_signup.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gradgigs/service/auth_validator.dart';
+import 'package:gradgigs/view/authentication/login.dart';
+import 'package:gradgigs/view/user_profile/rec_profile.dart';
 
 class RecruiterSignupPage extends StatefulWidget {
   final String role;
@@ -8,19 +11,394 @@ class RecruiterSignupPage extends StatefulWidget {
   State<RecruiterSignupPage> createState() => _RecruiterSignupPageState();
 }
 
-//body container - maroon
-// header "Job Recruiter"
-// input:
-// username
-// fulname
-// mobile number
-// Date of birth
-// Nationality - gender
-// role - Acedamician-organisation/club
-// sign up button
 class _RecruiterSignupPageState extends State<RecruiterSignupPage> {
+  final _formkey = GlobalKey<FormState>();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController comfirmPasswordController =
+      TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController fullnameController = TextEditingController();
+  bool _obscurePassword = true;
+  String? selectedCategory;
+
+  void recruiterSignUp() {
+    //get details recruiter
+    String email = emailController.text;
+    String password = passwordController.text;
+    String fullname = fullnameController.text;
+    String username = usernameController.text;
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => RecruiterProfilePage(
+                email: email,
+                password: password,
+                fullName: fullname,
+                username: username,
+                selectedRole: selectedCategory)));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Sign Up',
+          style: TextStyle(
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        toolbarHeight: 80,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(25),
+            bottomLeft: Radius.circular(25),
+          ),
+        ),
+        elevation: 5,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Card(
+                  color: const Color.fromARGB(255, 91, 0, 30),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Form(
+                      key: _formkey,
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 20),
+                            child: Text(
+                              'Job Recruiter',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Contrail One',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          //-------------------------------------EMAIL INPUT---------------------------------//
+                          const SizedBox(
+                            width: 300,
+                            child: Text(
+                              "Email",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            child: TextFormField(
+                              controller: emailController,
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 0),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                              ),
+                              validator: (value) =>
+                                  Validator.validateEmail(value!),
+                            ),
+                          ),
+                          //-------------------------------------PASSWORD INPUT---------------------------------//
+                          const SizedBox(
+                            width: 300,
+                            child: Text(
+                              "Password",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            child: TextFormField(
+                              controller: passwordController,
+                              obscureText: _obscurePassword,
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 0),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _obscurePassword
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _obscurePassword = !_obscurePassword;
+                                    });
+                                  },
+                                ),
+                              ),
+                              validator: (value) =>
+                                  Validator.validatePassword(value!),
+                            ),
+                          ),
+                          //-------------------------------------CONFIRM PASSWORD INPUT---------------------------------//
+                          const SizedBox(
+                            width: 300,
+                            child: Text(
+                              "Confirm Password",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            child: TextFormField(
+                              controller: comfirmPasswordController,
+                              obscureText: _obscurePassword,
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 0),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _obscurePassword
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _obscurePassword = !_obscurePassword;
+                                    });
+                                  },
+                                ),
+                              ),
+                              validator: (value) =>
+                                  Validator.validateComfirmPassword(
+                                      value!, passwordController.text),
+                            ),
+                          ),
+                          //-------------------------------------FULL NAME INPUT---------------------------------//
+                          const SizedBox(
+                            width: 300,
+                            child: Text(
+                              "Full Name",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            child: TextFormField(
+                              controller: fullnameController,
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 0),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                              ),
+                              validator: (value) =>
+                                  Validator.validateName(value!),
+                            ),
+                          ),
+                          //-------------------------------------USERNAME INPUT---------------------------------//
+                          const SizedBox(
+                            width: 300,
+                            child: Text(
+                              "Username",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            child: TextFormField(
+                              controller: usernameController,
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 0),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                              ),
+                              validator: (value) =>
+                                  Validator.validateName(value!),
+                            ),
+                          ),
+                          //-------------------------------------ROLE INPUT---------------------------------//
+                          const SizedBox(
+                            width: 300,
+                            child: Text(
+                              "Catergory",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 10),
+                              child: DropdownButtonFormField<String>(
+                                value: selectedCategory,
+                                decoration: InputDecoration(
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 0),
+                                  border: OutlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  hintText: "Please Choose a Category",
+                                ),
+                                items: ["Academician", "Organisation/Club"]
+                                    .map((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                onChanged: (String? newValue) {
+                                  setState(() {
+                                    selectedCategory = newValue;
+                                  });
+                                },
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please select a role';
+                                  }
+                                  return null; //if input
+                                },
+                              )),
+                          //-------------------------------------SIGN UP BUTTON---------------------------------//
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 16),
+                            child: Center(
+                              child: SizedBox(
+                                height: 50,
+                                width: 250,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    if (_formkey.currentState!.validate()) {
+                                      recruiterSignUp();
+                                    } else {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(const SnackBar(
+                                              content:
+                                                  Text('Please fill input')));
+                                    }
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    textStyle: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    foregroundColor: Colors.black,
+                                    backgroundColor: const Color.fromARGB(
+                                        255, 228, 185, 112),
+                                    shape: const RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(10)),
+                                    ),
+                                  ),
+                                  child: const Text('Sign up'),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 25),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        "Already have an account?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginPage(
+                                title: 'Sign in',
+                              ),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "Sign in",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 91, 0, 30),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/view/user_profile/apl_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ApplicantProfilePage extends StatefulWidget {
+  const ApplicantProfilePage({super.key});
+
+  @override
+  State<ApplicantProfilePage> createState() => _ApplicantProfilePageState();
+}
+
+class _ApplicantProfilePageState extends State<ApplicantProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
