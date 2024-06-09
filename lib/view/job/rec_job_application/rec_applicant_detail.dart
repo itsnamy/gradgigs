5a6c44d4e6710cb3adb1764fa6c9d805b70b28b3
AmmarJ23 <ammar.jmldn@outlook.com>
2024-06-09 15:07:03 +0800

    Update Path after application status

--- lib/view/job/rec_job_application/rec_applicant_detail.dart
+++ lib/view/job/rec_job_application/rec_applicant_detail.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:gradgigs/model/apl_profile_model.dart';
 import 'package:gradgigs/model/job_status_model.dart';
 import 'package:gradgigs/model/rec_job_model.dart';
+import 'package:gradgigs/navbar/rec_navbar.dart';
 import 'package:gradgigs/repository/job_repository/job_status_repository.dart';
 import 'package:gradgigs/view/job/rec_job_application/rec_applicant_list.dart';
 import 'package:http/http.dart' as http;
@@ -205,8 +206,8 @@ class _RecruiterApplicantDetailState extends State<RecruiterApplicantDetail> {
                             Navigator.push(
                               context,
                               MaterialPageRoute(
-                                builder: (context) => RecruiterApplicantList(
-                                  job: widget.job,
+                                builder: (context) => CustomBottomNavigationBar(
+                                  
                                 ),
                               ),
                             );
@@ -240,7 +241,11 @@ class _RecruiterApplicantDetailState extends State<RecruiterApplicantDetail> {
                             JobStatusRepository.instance.updateJobStatus(
                                 widget.application.statusId, "Rejected");
                             await _updateApplication(context, rejectedStatus);
-                            Navigator.pop(context);
+                            Navigator.push(context, MaterialPageRoute(
+                                builder: (context) => CustomBottomNavigationBar(
+                                  
+                                ),
+                              ),);
                           },
                           style: ElevatedButton.styleFrom(
                             textStyle: const TextStyle(
